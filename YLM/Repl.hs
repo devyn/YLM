070907c398eb894fe6d3cd3b51a6f8c068d7d028
Devyn Cairns <devyn.cairns@gmail.com>
2012-12-27 13:12:32 -0800

    Disable readline

--- YLM/Repl.hs
+++ YLM/Repl.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE GADTs #-}
+
 module YLM.Repl (StateDisplay(..), repl) where
 
 import YLM.Data
@@ -5,9 +7,10 @@ import YLM.Runtime
 import Data.Map (Map)
 import qualified Data.Map as Map
 import System.IO
-import System.Console.Readline
+--import System.Console.Readline
 
-data (TextInterface i, PrettyPrint i) => StateDisplay i = StateDisplay (i -> Scope -> (String, StateDisplay i))
+data StateDisplay i where
+  StateDisplay :: (TextInterface i, PrettyPrint i) => (i -> Scope -> (String, StateDisplay i)) -> StateDisplay i
 
 repl :: (TextInterface i, PrettyPrint i)
      => i              -- ^ The text interface/pretty printer to use
@@ -17,11 +20,17 @@ repl :: (TextInterface i, PrettyPrint i)
 
 repl i m (StateDisplay f) = do
   let (p, f') = f i m
-  ms <- readline $ concat ["\x01\ESC[1m\x02", p, ">>\x01\ESC[0m\x02 "]
+  --ms <- readline $ concat ["\x01\ESC[1m\x02", p, ">>\x01\ESC[0m\x02 "]
+  putStr $ concat ["\x01\ESC[1m\x02", p, ">>\x01\ESC[0m\x02 "]
+  hFlush stdout
+  ms <- getLine >>= \ s ->
+    case s of
+         "" -> return Nothing
+         _  -> return (Just s)
   case ms of
     Nothing -> repl i m f'
-    Just s  -> do
-      addHistory s
+    Just s  -> --do
+      --addHistory s
       case yread i "(interactive)" s of
         Left err -> do
           putStrLn $ concat [" \ESC[0;31mERROR: ", err, "\ESC[0m"]
